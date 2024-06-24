ebc0f55abdcdc2542cb301afe2c02bc6a4ff3685
cristiangar0398 <cristianagr1998@gmail.com>
2024-06-24 12:18:54 -0500

    concrete implementation of DB repository

--- repository/user.go
+++ repository/user.go
@@ -13,6 +13,7 @@ var (
 type UserRepository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id int64) (*models.User, error)
+	Close() error
 }
 
 func SetRepository(repository UserRepository) {
@@ -25,4 +26,8 @@ func InsertUser(ctx context.Context, user *models.User) error {
 
 func GetUserById(ctx context.Context, id int64) (*models.User, error) {
 	return implementation.GetUserById(ctx, id)
+}
+
+func Close() error {
+	return implementation.Close()
 }
